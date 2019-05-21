f9f726575fda8d1250f1a3dd2448f620295f3887
Adam <blekk2@gmail.com>
2019-05-21 22:05:49 +0200

    created implementation file to cardFromJson

--- src/lib/card_reader/cardFromJson.h
+++ src/lib/card_reader/cardFromJson.h
@@ -6,23 +6,9 @@
 
 namespace mtg_card {
 
-    void to_json(nlohmann::json& j, const Card& c) {
-        //TODO
-        j = nlohmann::json{{"name", c.name}, {"colors", c.colors}};
-    }
+    void to_json(nlohmann::json& j, const Card& c);
 
-    void from_json(const nlohmann::json& j, Card& c) {
-        j.at("name").get_to(c.name);
-        j.at("convertedManaCost").get_to(c.convertedManaCost);
-        if(j.contains("manaCost"))
-            c.manaCost = j.at("manaCost").get<std::string>();
-
-        for(auto& color : j.at("colors"))
-        {
-            //TODO
-            c.colors.emplace_back(color.get<std::string>());
-        }
-    }
+    void from_json(const nlohmann::json& j, Card& c);
 
 }
 
